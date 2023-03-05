4cb94b83f3ce2494b8d500b9093404ae643b90f4
Marcel Kurovski <marcel.kurovski@gmail.com>
2023-03-05 14:22:45 +0000

    feat: add autocomplete

--- week2/search.py
+++ week2/search.py
@@ -64,7 +64,25 @@ def autocomplete():
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
+
+            opensearch = get_opensearch()
+            query_obj = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion": {
+                            "field": "suggest",
+                            "skip_duplicates": True
+                        }
+                    }
+                }
+            }
+            
+            if type == "queries":
+                search_response = opensearch.search(body=query_obj, index="bbuy_queries")
+            else:
+                search_response = opensearch.search(body=query_obj, index="bbuy_products")
+
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
@@ -108,6 +126,7 @@ def query():
         ##### W2, L1, S2
         qu.add_click_priors(query_obj, user_query, prior_clicks)
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -123,6 +142,7 @@ def query():
         #### W2, L1, S2
         qu.add_click_priors(query_obj, user_query, prior_clicks)
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
 
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
